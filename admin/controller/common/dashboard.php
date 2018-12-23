4078aedd90c88c829e207031975e72378aa3c59b
Minh Tuan <minhtuan9922@gmail.com>
2018-12-24 01:52:52 +0700

    cập nhật

--- admin/controller/common/dashboard.php
+++ admin/controller/common/dashboard.php
@@ -95,4 +95,107 @@ class ControllerCommonDashboard extends Controller {
 
 		$this->response->setOutput($this->load->view('common/dashboard', $data));
 	}
+	public function chart_view() {
+		$this->load->language('extension/dashboard/chart');
+
+		$json = array();
+
+		$this->load->model('extension/dashboard/chart');
+
+		$json['order'] = array();
+		$json['customer'] = array();
+		$json['xaxis'] = array();
+
+		$json['order']['label'] = $this->language->get('text_order');
+		$json['customer']['label'] = $this->language->get('text_customer');
+		$json['order']['data'] = array();
+		$json['customer']['data'] = array();
+
+		if (isset($this->request->get['range'])) {
+			$range = $this->request->get['range'];
+		} else {
+			$range = 'day';
+		}
+
+		switch ($range) {
+			default:
+			case 'day':
+				$results = $this->model_extension_dashboard_chart->getTotalOrdersByDay();
+
+				foreach ($results as $key => $value) {
+					$json['order']['data'][] = array($key, $value['total']);
+				}
+
+				$results = $this->model_extension_dashboard_chart->getTotalCustomersByDay();
+
+				foreach ($results as $key => $value) {
+					$json['customer']['data'][] = array($key, $value['total']);
+				}
+
+				for ($i = 0; $i < 24; $i++) {
+					$json['xaxis'][] = array($i, $i);
+				}
+				break;
+			case 'week':
+				$results = $this->model_extension_dashboard_chart->getTotalOrdersByWeek();
+
+				foreach ($results as $key => $value) {
+					$json['order']['data'][] = array($key, $value['total']);
+				}
+
+				$results = $this->model_extension_dashboard_chart->getTotalCustomersByWeek();
+
+				foreach ($results as $key => $value) {
+					$json['customer']['data'][] = array($key, $value['total']);
+				}
+
+				$date_start = strtotime('-' . date('w') . ' days');
+
+				for ($i = 0; $i < 7; $i++) {
+					$date = date('Y-m-d', $date_start + ($i * 86400));
+
+					$json['xaxis'][] = array(date('w', strtotime($date)), date('D', strtotime($date)));
+				}
+				break;
+			case 'month':
+				$results = $this->model_extension_dashboard_chart->getTotalOrdersByMonth();
+
+				foreach ($results as $key => $value) {
+					$json['order']['data'][] = array($key, $value['total']);
+				}
+
+				$results = $this->model_extension_dashboard_chart->getTotalCustomersByMonth();
+
+				foreach ($results as $key => $value) {
+					$json['customer']['data'][] = array($key, $value['total']);
+				}
+
+				for ($i = 1; $i <= date('t'); $i++) {
+					$date = date('Y') . '-' . date('m') . '-' . $i;
+
+					$json['xaxis'][] = array(date('j', strtotime($date)), date('d', strtotime($date)));
+				}
+				break;
+			case 'year':
+				$results = $this->model_extension_dashboard_chart->getTotalOrdersByYear();
+
+				foreach ($results as $key => $value) {
+					$json['order']['data'][] = array($key, $value['total']);
+				}
+
+				$results = $this->model_extension_dashboard_chart->getTotalCustomersByYear();
+
+				foreach ($results as $key => $value) {
+					$json['customer']['data'][] = array($key, $value['total']);
+				}
+
+				for ($i = 1; $i <= 12; $i++) {
+					$json['xaxis'][] = array($i, date('M', mktime(0, 0, 0, $i)));
+				}
+				break;
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
 }
